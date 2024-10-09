58f8f11d6b1dadc20f18f2b3001f7ed71f584f82
Maksym Dzhosan <dzhomaks@gmail.com>
2024-10-09 09:24:21 +0300

    style(CreateTaskComponent,ButtonComponent): fix UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { provideHttpClient } from '@angular/common/http';
+import { CreateTaskModule } from 'features/create-task';
 import { UiModule } from 'shared/ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, UiModule],
+  imports: [BrowserModule, AppRoutingModule, UiModule, CreateTaskModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
