a61044f6a8328b1a44ffb74187db52bb059270e5
Maksym Dzhosan <dzhomaks@gmail.com>
2024-10-10 15:43:56 +0300

    chore: Add routes and components to template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from 'pages/not-found';
+import { TaskListComponent } from 'pages/task-list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskListComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
